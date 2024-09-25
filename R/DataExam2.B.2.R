1d8fc0930a47d69462fcff7494f9fb8e9ad222fc
Muhammad Yaseen <myaseen208@gmail.com>
2024-09-25 12:20:02 +0000

    version 0.2.0

--- R/DataExam2.B.2.R
+++ R/DataExam2.B.2.R
@@ -6,21 +6,27 @@
 #' @description Exam2.B.2 is used to visualize the effect of glm model statement with binomial data with logit and probit links.
 #' @format A \code{data.frame} with 11 rows and 3 variables.
 #' @details
-#'        \itemize{
+#'      \itemize{
 #'        \item x independent variable
-#'        \item n bernouli trials(bernouli outcomes on each individual)
-#'        \item y  number of successes on each individual
-#'        }
-#' @author \enumerate{
+#'        \item n bernouli trials (bernouli outcomes on each individual)
+#'        \item y number of successes on each individual
+#'      }
+#'
+#' @author
+#'  \enumerate{
 #'          \item  Muhammad Yaseen (\email{myaseen208@@gmail.com})
 #'          \item Adeela Munawar (\email{adeela.uaf@@gmail.com})
 #'          }
-#' @references \enumerate{
+#'
+#' @references
+#' \enumerate{
 #' \item Stroup, W. W. (2012).\emph{Generalized linear mixed models: modern concepts, methods and applications}.
 #'              CRC press.
 #'  }
+#'
 #' @seealso
-#'    \code{\link{Exam2.B.2}}
+#'    \link{Exam2.B.2}
+#'
 #' @examples
 #' data(DataExam2.B.2)
 NULL
